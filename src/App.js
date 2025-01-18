a615fd4a811d4c66393215859f94a442b634bdfc
ltlely <lylytr.311@gmail.com>
2025-01-18 17:11:49 -0500

    created file structure and made the base layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from "react";
+import Home from "./Components/Home";
 
 function App() {
+  const home = useRef(null);
+  const features = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    if (elementRef?.current) {
+      window.scrollTo({
+        top: elementRef.current.offsetTop,
+        bottom: elementRef.current.offsetTop,
+        behavior: "smooth",
+      });
+    } else {
+      console.error("Ref is null or undefined");
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="parent-container">
+          <div id="Frags">
+            <h1 ref={home}>Frags</h1>
+          </div>
+          <div id="content-header">
+            <h1 onClick={() => scrollToSection(features)}>Features</h1>
+            <h1>Pricing</h1>
+            <h1>Log in</h1>
+            <h1>Sign up</h1>
+          </div>
+      </div>
+
+      <Home />
+     
+      <div className="sections">
+        <div className="testimonials-section">
+          <h3 id="textimonialsText">Trusted by 100+ businesses</h3>
+        </div>
+
+        <div className="features-section" ref={features}>
+          <h3 id="featuresText">10x Your Gaming Content, 100 Hours Saved Monthly</h3>
+        </div>
+
+        <div className="faq-section">
+          <h3 id="faqText">FAQs</h3>
+        </div>
+
+        <div className="footer-section">
+          <h3 id="footerText">footer</h3>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
