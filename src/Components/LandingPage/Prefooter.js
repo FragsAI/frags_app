8157e06d8fce59596d14a38b319dd6c05826f1e2
Daho Mita <dahomita17062006@gmail.com>
2025-01-19 18:18:32 -0500

    changes in header margin and size + add Prefooter

--- src/Components/LandingPage/Prefooter.js
+++ src/Components/LandingPage/Prefooter.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import "../../Styles/LandingPageStyles/prefooter.css"
 
 const Prefooter = () => {
   return (
-    <div className="faq-section">
-          <h3 id="faqText">FAQs</h3>
+    <section className="prefooter">
+      <div className="prefooter-content">
+        <h1>Ready to Get Started</h1>
+        <p>Create an account or talk to one of our experts.</p>
+        <div className="prefooter-buttons">
+          <a href="#login" className="btn btn-login">LOGIN</a>
+          <a href="#signup" className="btn btn-signup">SIGNUP</a>
         </div>
+      </div>
+    </section>
   )
 }
 
