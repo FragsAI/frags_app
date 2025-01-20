4353d818139dea5752369208f8b9cf5566909c82
ltlely <lylytr.311@gmail.com>
2025-01-20 14:25:39 -0500

    adjust home css and credentials css

--- src/Components/LandingPage/Home.js
+++ src/Components/LandingPage/Home.js
@@ -10,19 +10,21 @@ const Home = () => {
             </div>
         </div>
 
-        <div className="hometextcontainer">
-            <div className="move-hometext">
-                <img className="hometextimg" src="./assets/homeText.png"/>
+        <div className="home-content">
+            <div className="hometextcontainer">
+                <div className="move-hometext">
+                    <img className="hometextimg" src="./assets/homeText.png"/>
+                </div>
             </div>
-        </div>
 
-        <div className="bar">
-            <div className='move-bar'>
-                <div className="innerbar">
-                    <div className="innerbar-input">
-                        <img className="bartext" src="../assets/bartext.png" alt="Bar Text" />
-                        <img className="createshorts" src="../assets/Group 305.png" alt="Create Shorts" />
-                        <img className="uploadvideo" src="../assets/Group 306.png" alt="Upload Video" />
+            <div className="bar">
+                <div className='move-bar'>
+                    <div className="innerbar">
+                        <div className="innerbar-input">
+                            <img className="bartext" src="../assets/bartext.png" alt="Bar Text" />
+                            <img className="createshorts" src="../assets/Group 305.png" alt="Create Shorts" />
+                            <img className="uploadvideo" src="../assets/Group 306.png" alt="Upload Video" />
+                        </div>
                     </div>
                 </div>
             </div>
