75f24c65dce5edaaaaabeff76e669338a51b7d22
Daho Mita <dahomita17062006@gmail.com>
2025-01-22 18:21:33 -0500

    changes to header and prefooter

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import Header from "./Header"
+import Header from "../Components/Header"
+import Footer from "../Components/Footer"
 import "./../Styles/product.css"
 
 const Pricing = () => {
   return (
     <>
-    
-    <div className="product-content">
-        <div className='move-featuresText'>
-            <img src="./assets/featuresText.png" />
-        </div>
-        <div className="features-featuresImg">
-            <img src="./assets/featuresColOne.png" />
-            <img src="./assets/featuresColOne.png" />
-        </div>
+    <div>
+      <Header/>
+      <Footer/>
     </div>
 
     </>
