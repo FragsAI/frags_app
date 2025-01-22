6f38a3d00a254d22b35c839655f8b6a59b4b6875
ltlely <lylytr.311@gmail.com>
2025-01-22 15:34:28 -0500

    added feature page

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from "./Header"
+import "./../Styles/pricing.css"
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <>
+    <Header />
+    
+    <div className="pricing-content">
+        <div className='move-featuresText'>
+            <img src="./assets/featuresText.png" />
+        </div>
+        <div className="features-featuresImg">
+            <img src="./assets/featuresColOne.png" />
+            <img src="./assets/featuresColOne.png" />
+        </div>
+    </div>
+
+    </>
   )
 }
 
