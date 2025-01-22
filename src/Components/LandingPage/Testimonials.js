e303d60d07ec3e84dda6c1e6e47441a8d32b55c0
Daho Mita <dahomita17062006@gmail.com>
2025-01-22 18:37:03 -0500

    add testimonals again

--- src/Components/LandingPage/Testimonials.js
+++ src/Components/LandingPage/Testimonials.js
@@ -4,58 +4,35 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../Styles/LandingPageStyles/testimonials.css"; 
 
+const imageUrls = [
+  "../../assets/Base.png",
+  "../../assets/Base (1).png",
+  "../../assets/Base (1).png",
+"../../assets/Base (1).png",
+  "../../assets/Base.png",
+  "../../assets/Base (1).png",
+  "../../assets/Base.png",
+  "../../assets/Base (1).png",
+  "../../assets/Base (1).png",
+"../../assets/Base (1).png",
+  "../../assets/Base.png",
+  "../../assets/Base (1).png",
+];
+
 const Testimonials = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    centerMode: true,
-    centerPadding: "0px",
-    customPaging: () => <div className="custom-dot"></div>,
-    appendDots: (dots) => (
-      <div style={{ bottom: "20px" }}>
-        <ul style={{ margin: "0px", padding: "0px" }}>{dots}</ul>
-      </div>
-    ),
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          centerPadding: "100px",
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          centerPadding: "50px",
-        },
-      },
-    ],
-  };
-
-  const quotes = [
-    { text: "“Had a Frag'n time truly recommend!”", author: "John Doe" },
-    { text: "“Excellent service and amazing support team!”", author: "Jane Smith" },
-    { text: "“I couldn’t be happier with my experience!”", author: "Alex Johnson" },
-  ];
-
   return (
-    <div className="testimonials-container">
-      <h2 className="testimonials-title">
-        <span className="graygradiant">See why so many creators</span> <span className="orangegradiant">love Frags</span>
-      </h2>
-      <div className="testimonials-slider">
-        <Slider {...settings}>
-          {quotes.map((quote, index) => (
-            <div key={index} className="testimonial-quote">
-              <p className="graygradiant">{quote.text}</p>
-              <p className="graygradiant author">{quote.author}</p>
-            </div>
-          ))}
-        </Slider>
+    <div className="testimonials">
+      <h2 className="testimonials-header">See why so many creators <span className="orangegradiant">love Frags</span></h2>
+      <div className="testimonials-container">
+        {Array.from({ length: 4 }).map((_, colIndex) => (
+          <div key={colIndex} className="testimonial-column">
+            {imageUrls.slice(colIndex * 3, colIndex * 3 + 3).map((url, imgIndex) => (
+              <div key={imgIndex} className="testimonial-image-wrapper">
+                <img src={url} alt={`Testimonial ${colIndex}-${imgIndex}`} className="testimonial-image" />
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
