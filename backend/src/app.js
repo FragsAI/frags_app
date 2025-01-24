f19ca7471b65ebfed240a28cd6098a631a183e87
Jtolen543 <Jtolen543@gmail.com>
2025-01-24 16:34:22 -0500

    Adding testing router for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import * as middleware from './utils/middleware';
+import subscriptionRouter from './controllers/stripe/subscription';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+
+
+app.use('/api/subscription', subscriptionRouter);
 
 app.use(middleware.unknownEndpoint);
 
--- backend/src/controllers/Stripe/pricingPlan.js
+++
@@ -1,5 +0,0 @@
-import stripe from ".../config/stripe"
-
-const pricingPlan = async (req, res) => {
-    
-}
