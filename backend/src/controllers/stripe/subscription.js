6440c6414ee4d9f259de8ed2e844a2a9ec791ceb
Jtolen543 <Jtolen543@gmail.com>
2025-01-26 11:54:50 -0500

    Modified subscription router

--- backend/src/controllers/stripe/subscription.js
+++ backend/src/controllers/stripe/subscription.js
@@ -1,31 +1,41 @@
 import stripe from "../../config/stripe";
 import express from 'express';
+import supabase from "../../config/supabase";
+import { clerkMiddleware, requireAuth, clerkClient } from "@clerk/express";
 const subscriptionRouter = express.Router();
 
-subscriptionRouter.post('/create', async (req, res) => {
-    const { email, payment_method } = req.body;
-    const customer = await stripe.customers.create({
-        email,
-        payment_method
-    });
-    const subscription = await stripe.subscriptions.create({
-        customer: customer.id,
-        items: [{ price: process.env.STRIPE_PRICE_ID }]
-    });
-    res.json(subscription);
-});
-
 subscriptionRouter.post('/test-payment-intent', async (req, res) => {
-    const { amount } = req.body;
+    const amount = req.body.amount;
     try {
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency: 'usd',
         });
-        return res.status(200).send(paymentIntent);
+        res.status(200).send(paymentIntent);
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
+});
+
+subscriptionRouter.use(clerkMiddleware());
+subscriptionRouter.use(requireAuth());
+
+subscriptionRouter.post('/', async (req, res) => {
+    const user = await clerkClient.users.getUser(req.body.user_id);
+    const customer = await stripe.customers.search({
+        query: `email:${user.primaryEmailAddress.emailAddress}`
+    }).data[0] || await stripe.customers.create({
+        email: user.primaryEmailAddress.emailAddress,
+        name: `${user.firstName} ${user.lastName}`,
+    })
+
+    const price = await stripe.prices.retrieve(req.body);
+    
+    const subscription = await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ price: req.body }]
+    });
+    return res.json(subscription);
 });
 
 subscriptionRouter.get("/test", (req, res) => {
