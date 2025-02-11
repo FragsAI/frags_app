933790349f12d8cbeaa6a897007137f53a93be6e
Nikola Robinsomn <nikola.robinson@outlook.com>
2025-02-11 15:09:43 -0500

    Somewhat functional upload feature

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -1,19 +1,21 @@
-import { createClient } from '@supabase/supabase-js'
-import { clerkClient } from '@clerk/express'
-import config from '../utils/config'
+// supabase.js
+import { createClient } from '@supabase/supabase-js';
+import { clerkClient } from '@clerk/express';
+import config from '../utils/config.js';
 
 const createSupabase = async (sessionID) => {
-    const clerkToken = await clerkClient.sessions.getToken(sessionID, "supabase-jwt")
-    const supabase = createClient(
-        config.DB_URI,
-        config.DB_KEY,
-        {
-            global: {
-                headers: { Authorization: `Bearer ${clerkToken.jwt}`}
-            }
-        }
-    )
-    return supabase
-}
+  const clerkToken = await clerkClient.sessions.getToken(sessionID, "supabase-jwt");
 
-export default createSupabase
+  const supabase = createClient(
+    config.DB_URI,
+    config.DB_KEY,
+    {
+      global: {
+        headers: { Authorization: `Bearer ${clerkToken.jwt}` }
+      }
+    }
+  );
+  return supabase;
+};
+
+export default createSupabase;
